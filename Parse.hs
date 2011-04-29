91d440220aa616512d13bb6c05b37e8004cf13ea
Nathan Collins <nathan.collins@gmail.com>
2011-04-29 14:58:01 -0700

    Haskell version of Parse

---
+++ Parse.hs
@@ -0,0 +1,49 @@
+module Parse where
+
+import Prelude hiding (lines)
+import Text.Parsec -- ParserCombinators.Parsec -- hiding ((<|>))
+-- import Text.Parsec.Token -- semi, colon, lexeme, natural ...
+import Text.ParserCombinators.Parsec (parseFromFile)
+import Control.Applicative hiding ((<|>), many)
+
+-- For each contructor the first int is the node number and
+-- the other ints are edge numbers with the first being the trunk
+data PNode = PEndPoint Int Int
+           | PSwitch Int Int Int Int
+             deriving Show
+
+-- ??? how to do lexeme version tim described (Text.Parsec.Token) ?
+--     
+--     many (lexeme line)
+--
+--     natural
+--
+--     natural <* semi
+--     natural <* colon
+--     natural <* dot -- ???
+
+--num = natural -- read <$> many1 digit
+num = read <$> many1 digit
+num_ c = num <* char c
+pEndPoint = PEndPoint <$> num_ ':'
+                      <*> num_ '.'
+pSwitch = PSwitch <$> num_ ':' -- ':' 
+                  <*> num_ ';' 
+                  <*> num_ ',' 
+                  <*> num
+line = try pSwitch <|> pEndPoint
+
+-- Parse each line and return a list.
+lines =  (line `sepEndBy` spaces) <* eof
+
+-- Parse the text in a file and return a parsed list in the IO monad.
+file f = do p <- parseFromFile lines f
+            case p of 
+              Left e -> error $ show e -- ??? what's the right way to fail on parse error
+              Right p -> return p
+
+main = do
+       d <- file "sample1.txt"
+       print d
+       e <- file "sample2.txt"
+       print e
